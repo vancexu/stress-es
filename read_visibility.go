6eee35601a6fd1d72169273dcafe93ce268775ba
Bowei Xu <boweixu@uber.com>
2019-02-21 11:55:34 -0800

    Support multi-thread

--- read_visibility.go
+++ read_visibility.go
@@ -7,6 +7,7 @@ import (
 	"math/rand"
 	"time"
 	"github.com/vancexu/stress-es/common"
+	"sync"
 )
 
 func read_visibility(client *elastic.Client, low, high int64, from, pagesize int) (int64, int64) {
@@ -40,28 +41,44 @@ func main() {
 	var timeBackInHours int
 	fmt.Println("Number of hours back: ")
 	fmt.Scanln(&timeBackInHours)
+	var numOfThreads int
+	fmt.Println("Number of threads: ")
+	fmt.Scanln(&numOfThreads)
+	if numOfThreads == 0 {
+		numOfThreads = 1
+	}
 
 	client, err := common.NewElasticClient()
 	if err != nil {
 		panic(err)
 	}
 
-	var totalTime int64
-	var totalHits int64
-	testStartTime := time.Now()
-	for i := 0; i < times; i += 1 {
-		lo := time.Now().Add(time.Duration(-timeBackInHours*60)*time.Minute).UnixNano()
-		hi := time.Now().UnixNano()
-		src := rand.NewSource(lo)
-		r := rand.New(src)
-		t, h := read_visibility(client, lo, hi, r.Intn(10), 100)
-		//fmt.Println(t)
-		totalTime += t
-		totalHits += h
+	fn := func(id int, wg *sync.WaitGroup) {
+		var totalTime int64
+		var totalHits int64
+
+		for i := 0; i < times; i += 1 {
+			lo := time.Now().Add(time.Duration(-timeBackInHours*60)*time.Minute).UnixNano()
+			hi := time.Now().UnixNano()
+			src := rand.NewSource(lo)
+			r := rand.New(src)
+			t, h := read_visibility(client, lo, hi, r.Intn(10), 100)
+			//fmt.Println(t)
+			totalTime += t
+			totalHits += h
+		}
+		fmt.Println(id)
+		fmt.Println("avg read time millis: ", totalTime/int64(times))
+		fmt.Println("avg hits: ", totalHits/int64(times))
+		wg.Done()
 	}
 
+	wg := &sync.WaitGroup{}
+	wg.Add(numOfThreads)
+	testStartTime := time.Now()
+	for i := 0; i < numOfThreads; i++ {
+		go fn(i, wg)
+	}
+	wg.Wait()
 	fmt.Println(time.Since(testStartTime))
-	fmt.Println("avg read time millis: ", totalTime/int64(times))
-	fmt.Println("avg hits: ", totalHits/int64(times))
-
 }
